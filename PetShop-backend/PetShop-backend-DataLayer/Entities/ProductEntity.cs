6d77c681463fda86e92c96b6dfe28e45feaf61fc
Amin Abedi <aminabd1617@gmail.com>
2024-10-03 11:45:38 +0330

    create entities and context

---
+++ PetShop-backend/PetShop-backend-DataLayer/Entities/ProductEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop_backend_DataLayer.Entities
+{
+    public class ProductEntity
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
